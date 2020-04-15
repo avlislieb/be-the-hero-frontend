32a7185b28d6c52f2d799c51ca381a3418e479bf
avlislieb <sr.silvagabriel@gmail.com>
2020-04-15 19:08:52 -0300

    insert google analytics

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import Routes from './routes';
-import './global.css'
+import './global.css';
+import ReactGa from 'react-ga';
+
 function App() {
-    
+  useEffect(() => {
+    ReactGa.initialize('UA-163765351-1');
+
+    //to report page view 
+    ReactGa.pageview('/');
+  }, [])  
+
     return (
       <div>
           <Routes /> 
